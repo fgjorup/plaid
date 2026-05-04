dfcca6f582e47a73dd79387d89f8b88a9915aa19
Frederik H Gjørup <fgjorup@chem.au.dk>
2026-05-04 11:20:46 +0200

    bump to version 1.1.0

--- plaid/__init__.py
+++ plaid/__init__.py
@@ -8,13 +8,13 @@ Author: Frederik H. Gjørup
 Institution: Aarhus University, Denmark & MAX IV Laboratory, Lund University, Sweden
 """
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'Frederik H Gjørup'
 __authors__ = ['Frederik H. Gjørup']
 __email__ = 'fgjorup@chem.au.dk'
 __organization__ = 'Aarhus University & MAX IV Laboratory'
 __institution__ = 'Department of Chemistry, Aarhus University, Denmark & MAX IV Laboratory, Lund University, Sweden'
-__date__ = '19.01.2026'
+__date__ = '04.05.2026'
 __year__ = '2026'
 __license__ = 'GPL-3.0'  # GNU General Public License v3.0
 __url__ = 'https://github.com/fgjorup/plaid'
